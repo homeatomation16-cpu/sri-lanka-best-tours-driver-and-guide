b7f4f9782957b30c47d915e824612b29ce29612d
homeatomation16-cpu <homeatomation16@gmail.com>
2026-04-01 12:10:12 +0530

    Update page.tsx

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,15 +1,15 @@
 // src/app/[locale]/page.tsx
-import HeroVideo from "../../components/HeroVideo";
-import Destinations from "../../components/Destinations";
-import SeasonalTours from "../../components/SeasonalTours";
-import OneDayTours from "../../components/OneDayTours";
-import SpecialOffer from "../../components/SpecialOffer";
-import TailorMade from "../../components/TailorMade";
-import WhyUs from "../../components/WhyUs";
-import Testimonials from "../../components/Testimonials";
-import Gallery from "../../components/Gallery";
-import VehiclesSection from "../../components/Vehicle";
-import Tours from "../../components/Tours";
+import HeroVideo from "@/components/HeroVideo";
+import Destinations from "@/components/Destinations";
+import SeasonalTours from "@/components/SeasonalTours";
+import OneDayTours from "@/components/OneDayTours";
+import SpecialOffer from "@/components/SpecialOffer";
+import TailorMade from "@/components/TailorMade";
+import WhyUs from "@/components/WhyUs";
+import Testimonials from "@/components/Testimonials";
+import Gallery from "@/components/Gallery";
+import VehiclesSection from "@/components/Vehicle";
+import Tours from "@/components/Tours";
 
 export default function HomePage() {
   return (
